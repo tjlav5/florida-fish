0bc17790942bd73e993ac7cf3da308974ac075c2
tjlav5 <tj@lavelle.io>
2019-04-06 23:28:21 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'fl-app',
@@ -6,4 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent  {
+  name = 'Angular';
+
+  constructor (
+    private metaService: Meta
+  ) { }
+
+  public setCanScale(canScale: boolean): void {
+    // this.metaService.removeTag('name=viewport');
+    if (canScale) {
+      this.metaService.updateTag({
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1.0, user-scalable=yes'
+      }, 'name=viewport');
+    } else {
+      this.metaService.updateTag({
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+      }, 'name=viewport');
+    }
+  }
+
 }
